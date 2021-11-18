898985ded67777753f974ba4cc106392807e1b54
meuriC <94645356+meuriC@users.noreply.github.com>
2021-11-18 21:53:14 +0000

    Add files via upload
    build files

---
+++ Paint/build/generated/source/buildConfig/debug/com/example/paint/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.paint;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.paint";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  public static final String MAPS_API_KEY = "AIzaSyCaRBeKkAQ_HIU4vQ9rfBd0WLy5Whax7go";
+}
